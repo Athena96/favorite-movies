2eb1781a5b380a76361fe79cdda1ec4899e3eadd
Jared Franzone <jffranzo@amazon.com>
2021-07-16 14:35:52 -0700

    next11

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  images: {
+    domains: ['nextjs.org']
+  },
     env: {
       API_HOST: process.env.API_HOST
     }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
 import Layout from '../components/layout'
 import Image from 'next/image'
+import Script from 'next/script'
+import myimage from '../public/tst.png'
 
 const Home = ({ data }) => {
 
@@ -15,13 +17,28 @@ const Home = ({ data }) => {
       </Head>
 
       <Image
-        src="/tst.png"
-        alt="Picture of the author"
-        width={500}
-        height={500}
-      />
+        src={myimage}
+        placeholder="blur"
+        alt="Picture of the"
+      /><br/>
+      <Image
+  src="https://nextjs.org/static/images/learn.png"
+  blurDataURL="data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAb/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWEREiMxUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
+  alt="Picture of the author"
+  placeholder="blur"
+  width='300px'
+  height='300px'
+/>
+<Script
+  src={'https://polyfill.io/v3/polyfill.min.js?features=Array.prototype.map'} // consent mangagement
+  strategy="beforeInteractive"
+  onLoad={() => {
+    // If loaded successfully, then you can load other scripts in sequence
+    console.log('loaded!');
+  }}
+/>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Favorite Movies - Dynamic (SSR) - {process.env.API_HOST}</h2>
+        <h2 className={utilStyles.headingLg}>Favorite Movies - Dynamic (SSG) - {process.env.API_HOST}</h2>
         <ul className={utilStyles.list}>
           {data.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
