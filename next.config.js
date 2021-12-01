72051d1058a9b071f8a97712419ebb6e23ff3bd4
Jared Franzone <jffranzo@amazon.com>
2021-12-01 13:47:13 -0800

    use 4:

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack')
 const { parsed: myEnv } = require('dotenv').config()
 module.exports = {
     reactStrictMode: true,
+    webpack5: false,
     webpack(config) {
         config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
         return config
