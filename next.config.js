680b5e50b9fb57580b664bb8f7536e382cd8342e
Jared Franzone <jffranzo@amazon.com>
2021-12-01 13:49:57 -0800

    tst

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 const webpack = require('webpack')
 const { parsed: myEnv } = require('dotenv').config()
+
+console.log('database host:', process.env.DATABASE_HOST);
+console.log('database port:', process.env.DATABASE_PORT);
+console.log('database user:', process.env.DATABASE_USER);
+console.log('database pass:', process.env.DATABASE_PASS);
+console.log('database name:', process.env.DATABASE_NAME);
+
 module.exports = {
     reactStrictMode: true,
     webpack5: false,
